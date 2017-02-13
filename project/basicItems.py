6ed4da34c75d170a483de05fd8cc13abe63f94c2
Alfred-Guo <alfred_guo@tom.com>
2017-02-13 09:50:23 +0800

    Delete basicItems.py

--- project/basicItems.py
+++
@@ -1,1 +0,0 @@
-# -*- coding: utf-8 -*-"""Created on Fri Nov 18 16:41:14 2016@author: Alfred"""import scrapyclass basicItem(scrapy.Item):    create_date = scrapy.Field()    update_date = scrapy.Field()    url = scrapy.Field()    class foo(scrapy.Item):
